bc48fde056f5510ae4586ed33699d4092717943a
giu176 <62891107+giu176@users.noreply.github.com>
2023-10-25 13:12:07 +0200

    Create natpmpc_script.sh

---
+++ natpmpc_script.sh
@@ -0,0 +1,18 @@
+while true; do
+    date
+
+    # Run natpmpc for UDP and TCP, redirect output to a temporary file
+    natpmpc -a 1 0 udp 60 -g 10.2.0.1 && natpmpc -a 1 0 tcp 60 -g 10.2.0.1 > /tmp/natpmpc_output || { 
+        echo -e "ERROR with natpmpc command \a" 
+        break
+    }
+
+    # Extract the port numbers from the output and save them in variables
+    port=$(grep 'TCP' /tmp/natpmpc_output | grep -o 'Mapped public port [0-9]*' | awk '{print $4}')
+
+    echo "Opened port: $port"
+    # Get the current --torrenting-port from the service file
+    current_port=$(grep -Po --max-count=1 '(?<=--torrenting-port=)[0-9]+' "$service_file")
+
+    sleep 45
+done
