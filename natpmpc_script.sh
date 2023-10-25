33331225c56b7d5755dd179092eb84510c459697
giu176 <62891107+giu176@users.noreply.github.com>
2023-10-25 13:20:26 +0200

    Update natpmpc_script.sh

--- natpmpc_script.sh
+++ natpmpc_script.sh
@@ -2,13 +2,13 @@ while true; do
     date
 
     # Run natpmpc for UDP and TCP, redirect output to a temporary file
-    natpmpc -a 1 0 udp 60 -g 10.2.0.1 && natpmpc -a 1 0 tcp 60 -g 10.2.0.1 > /tmp/natpmpc_output || { 
+    natpmpc -a 1 0 udp 60 -g 10.2.0.1 && natpmpc -a 1 0 tcp 60 -g 10.2.0.1 > ./natpmpc_output || { 
         echo -e "ERROR with natpmpc command \a" 
         break
     }
 
     # Extract the port numbers from the output and save them in variables
-    port=$(grep 'TCP' /tmp/natpmpc_output | grep -o 'Mapped public port [0-9]*' | awk '{print $4}')
+    port=$(grep 'TCP' ./natpmpc_output | grep -o 'Mapped public port [0-9]*' | awk '{print $4}')
 
     echo "Opened port: $port"
     sleep 45
